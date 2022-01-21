114a9a15fcce470514f9a88248efdfc22a9baa1d
judabne <judabne@hotmail.com>
2022-01-21 16:19:31 +0200

    allowing basic navigation between slides using the left and right button

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
     <div>
       <h5 style={{textAlign: "center"}}>Carousel</h5>
       <Carousel>
-        <img src="https://wallpaperaccess.com/full/5792878.jpg" />
-        <img src="https://wallpaperaccess.com/full/421006.jpg" />
+        <img src="/images/5792878.webp" />
+        <img src="/images/VzEtvB.jpg" />
+        <img src="/images/421006.webp" />
         <h1 style={{background: "#206ebd"}}>Sample h1</h1>
-        <img src="https://images.all-free-download.com/images/graphicthumb/beautiful_scenery_02_hd_picture_166319.jpg" />
+        <img src="/images/beautiful_scenery_02_hd_picture_166319.jpg" />
       </Carousel>
     </div>
   );
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,8 +1,45 @@
-import React, {Children} from 'react';
+import React, {Children, useState} from 'react';
 import classes from './Carousel.module.css'
 
+const widthSpan = 100.1
+
 function Carousel(props) {
+    const [sliderPosition, setSliderPosition] = useState(0);
     const { children } = props;
+
+    const prevSlideHandler = () => {
+        let newPosition = sliderPosition;
+        if (newPosition > 0) {
+            newPosition = newPosition - 1;
+        }
+        translateFullSlides(newPosition);
+        setSliderPosition(newPosition);
+    }
+
+    const nextSlideHandler = () => {
+        let newPosition = sliderPosition;
+        if (newPosition < children.length - 1) {
+            newPosition = newPosition + 1;
+        }
+        translateFullSlides(newPosition);
+        setSliderPosition(newPosition);
+    }
+
+    const prevClickHandler = () => {
+        prevSlideHandler();
+    }
+
+    const nextClickHandler = () => {
+        nextSlideHandler();
+    }
+
+    const translateFullSlides = (newPosition) => {
+        let toTranslate = -widthSpan * newPosition;
+        for (var i = 0; i < children.length; i++) {
+            let elem = document.getElementById(`carouselitem` + i);
+            elem.style.transform = `translateX(` + toTranslate +`%)`; 
+        }
+    }
 
     const displayItems = Children.map(children, (child, index) =>(
         <div className={classes.CarouselItem} id={`carouselitem` + index}>{child}</div>
@@ -11,11 +48,11 @@ function Carousel(props) {
     return (
         <div>
             <div className={classes.Container}>
-                <div className={classes.LeftArrow}>❰</div>
+                <div className={classes.LeftArrow} onClick={prevClickHandler}>❰</div>
                 <div className={classes.DisplayFrame}>
                     {displayItems}
                 </div>
-                <div className={classes.RightArrow}>❱</div>
+                <div className={classes.RightArrow} onClick={nextClickHandler}>❱</div>
             </div>
         </div>
     )
