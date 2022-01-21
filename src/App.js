1a6d39623bfa46322a5fd943204a0e3664c72b3f
judabne <judabne@hotmail.com>
2022-01-21 13:32:20 +0200

    displaying children of carousel

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
   return (
     <div>
       <h5 style={{textAlign: "center"}}>Carousel</h5>
-      <Carousel></Carousel>
+      <Carousel>
+        <img src="https://wallpaperaccess.com/full/5792878.jpg" />
+        <img src="https://wallpaperaccess.com/full/421006.jpg" />
+        <h1 style={{background: "#206ebd"}}>Sample h1</h1>
+        <img src="https://images.all-free-download.com/images/graphicthumb/beautiful_scenery_02_hd_picture_166319.jpg" />
+      </Carousel>
     </div>
   );
 }
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {Children} from 'react';
 import classes from './Carousel.module.css'
 
 function Carousel(props) {
+    const { children } = props;
+
+    const displayItems = Children.map(children, (child, index) =>(
+        <div className={classes.CarouselItem} id={`carouselitem` + index}>{child}</div>
+    ))
+
     return (
         <div>
             <div className={classes.Container}>
                 <div className={classes.LeftArrow}>❰</div>
-                <div className={classes.DisplayFrame}></div>
+                <div className={classes.DisplayFrame}>
+                    {displayItems}
+                </div>
                 <div className={classes.RightArrow}>❱</div>
             </div>
         </div>
